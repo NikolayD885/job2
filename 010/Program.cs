213d190bcafca8d9e311b62f6553c93ed0f74216
NikolayD <kolik885@ya.ru>
2022-08-13 15:06:26 +0700

    Программа к заданию №10

---
+++ 010/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа которая показывает вторую цифру трехзначного числа
+
+Console.WriteLine("Введите число");
+
+string? numberString1 = Console.ReadLine();
+
+int number1 = int.Parse(numberString1!);
+
+int number2 = ((number1 / 10) % 10);
+
+Console.WriteLine("Вторая цифра числа " + number2);
