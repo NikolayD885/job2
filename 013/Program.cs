fed8ebd5eaa445c13d72722719419dc41e9b9dea
NikolayD <kolik885@ya.ru>
2022-08-13 17:33:27 +0700

    Программа к заданию №13

---
+++ 013/Program.cs
@@ -0,0 +1,24 @@
+﻿
+   // Программа которая выводит третью цифру числа
+   Console.WriteLine("Введите число");
+
+   string? numberString1 = Console.ReadLine();
+
+   int number = int.Parse(numberString1!);
+
+    if (number < 99)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    else
+        {
+
+            while (number >= 1000)
+            {
+                number = number / 10;
+            }
+            int number1 = number % 10;
+
+            Console.WriteLine("Третья цифра числа " + number1);
+        }
+    
