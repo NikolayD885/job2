aa51b211cb336e9e5ebbfad748181ebc21fcb1b1
NikolayD <kolik885@ya.ru>
2022-08-13 18:05:52 +0700

    Программа к заданию №15

---
+++ 015/Program.cs
@@ -0,0 +1,39 @@
+﻿// Программа которая определяет является ли день недели выходным
+Console.WriteLine("Введите номер дня недели");
+
+string? numberString1 = Console.ReadLine();
+
+int number = int.Parse(numberString1!);
+
+if (number == 1)
+{
+    Console.WriteLine("Рабочий день");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Рабочий день");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Рабочий день");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Рабочий день");
+}
+else if (number == 5)
+{
+    Console.WriteLine("Рабочий день");
+}
+else if (number == 6)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (number == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else 
+{
+   Console.WriteLine("Такого дня недели не существует");
+}
